669f855b17f1b70ff13c61efd27b6835749ad20a
Olivia Kesler <oakesler@protonmail.ch>
2020-01-15 17:58:43 +0000

    config file and application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,1 +1,10 @@
-#application controller
+class ApplicationController < Sinatra::Base  
+  configure do 
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+  
+  get '/' do 
+    erb :index
+  end
+end
--- config.ru
+++ config.ru
@@ -1,1 +1,8 @@
 require_relative './config/environment'
+
+if ActiveRecord::Migrator.needs_migration?
+  raise "Migrations are pending.Run 'rake db:migrate' to"
+end
+
+use Rack::MethodOverride
+run ApplicationController
