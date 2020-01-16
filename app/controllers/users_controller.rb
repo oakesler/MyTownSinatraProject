f200103f043582bc997d343beb2c7a74dbdc972f
Olivia Kesler <oakesler@protonmail.ch>
2020-01-16 17:54:52 +0000

    added sign up form and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
-  ###
+  
+  ########## THIS IS ALL SIGN UP ###############
+  get '/signup' do 
+    erb :'users/new.html'
+  end
+  
+  post 'users' do 
+    @user = User.new 
+    @user.email = params[:email]
+    @user.password = params[:password]
+    if @user.save
+      redirect "/login"
+    else 
+      erb :'users/new.html'
+    end
+  end
+  ##########THIS IS ALL SIGN UP ###################
 end 
   
