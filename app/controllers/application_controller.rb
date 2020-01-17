a478e02b0ae2d25516f62dadd3fd95054b2db893
Olivia Kesler <oakesler@protonmail.ch>
2020-01-17 01:05:35 +0000

    debugged application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,8 @@ helpers do
   end
   
   def current_user
-    @current_user ||= User.find_by(:email => session[:email]) if session[:email])
+    @current_user ||= User.find_by(:email => session[:email]) if session([:email])
+  end
    
   def login(email, password)
     #is the user who they claim to be?
@@ -30,6 +31,5 @@ helpers do
     #emailing them to let them know they logged out
     redirect "/posts"
   end
-   
  end
 end
