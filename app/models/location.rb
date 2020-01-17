5665a577d36a4d36fb40d881a687c666d22f9e68
Olivia Kesler <oakesler@protonmail.ch>
2020-01-17 23:20:22 +0000

    completed first draft of location location type models integration

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,5 +2,5 @@ class Location < ActiveRecord::Base
   belongs_to :user
   belongs_to :guide
   belongs_to :city
-  has_many :types
+  has_many :location_location_types
 end
--- app/models/location_type.rb
+++ app/models/location_type.rb
@@ -1,3 +1,4 @@
 class LocationType < ActiveRecord::Base
-  has_many :locations
+  has_many :location_location_types
+  has_many :locations through: :location_location_types
 end
