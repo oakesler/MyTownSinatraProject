11c3a5954c510c3b64082620cfef13a457a3da96
Olivia Kesler <oakesler@protonmail.ch>
2020-01-18 09:00:49 +0000

    adding some footer links

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,4 +4,11 @@ class LocationsController < Sinatra::Base
     erb :"locations/index"
   end
   
+  post '/locations/view/:id' do 
+    @guide = Guide.find(params[:id])
+    @user = User.find(@guide.user_id)
+    @city = City.find(@guide.city_id)
+    erb :"guides/view"
+  end
+  
 end
