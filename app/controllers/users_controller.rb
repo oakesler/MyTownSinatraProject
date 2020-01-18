92b6c66baba7c70c2868a7fab8bce96e3d5903df
Olivia Kesler <oakesler@protonmail.ch>
2020-01-18 01:27:54 +0000

    first draft of new guide form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,7 @@ class UsersController < ApplicationController
     end
     @city.save
     @guide = Guide.new(name: params["guide"]["name"], user_id: params[:id], city_id: @city.id)
+    @guide.save
     if params[:location_1].keys.include?("location_name_ids")
       params["location_1"]["location_name_ids"].each do |item|
         temp_location = Location.find(item)
@@ -54,6 +55,7 @@ class UsersController < ApplicationController
     else 
       @location_1 = Location.new(name: params["location_1"]["name"], address: params["location_1"]["address"], city_id: @city.id, user_id: params[:id], guide_id: @guide.id, type: params["location_1"]["type"])
     end
+    @location_2.save
     #-------------------------------------------------------------------#
     if params[:location_1].keys.include?("location_name_ids")
       params["location_1"]["location_name_ids"].each do |item|
@@ -63,6 +65,7 @@ class UsersController < ApplicationController
     else 
       @location_2 = Location.new(name: params["location_2"]["name"], address: params["location_2"]["address"], city_id: @city.id, user_id: params[:id], guide_id: @guide.id, type: params["location_2"]["type"])
     end
+    @location_2.save
     #-------------------------------------------------------------------#
     if params[:location_3].keys.include?("location_name_ids")
       params["location_3"]["location_name_ids"].each do |item|
@@ -71,60 +74,49 @@ class UsersController < ApplicationController
       end
     else 
       @location_3 = Location.new(name: params["location_3"]["name"], address: params["location_3"]["address"], city_id: @city.id, user_id: params[:id], guide_id: @guide.id, type: params["location_3"]["type"])
+      @location_3.save
     end
-  
-    
-    if params[:city].keys.include?("city_ids")
-        
-  
-    if params[:figure].keys.include?("title_ids")
-      params["figure"]["title_ids"].each do |item|
-        @title = Title.find(item)
-        @figure.titles << @title
-      end
-    end
-    @city = City.new(name: params["city"]["name"])
-    @guide = Guide.new(name: params["guide"]["name"], user_id: params[:id], city_id: @city.id)
-    
-    
-    #@location_1 = Location.new(name: params["location_1"]["name"], address: params["location_1"]["address"], city_id: @city.id, user_id: params[:id], guide_id: @guide.id, type: params["location_1"]["type"])
-    #@location_2 = Location.new(name: params["location_2"]["name"], address: params["location_2"]["address"], city_id: @city.id, user_id: params[:id], guide_id: @guide.id, type: params["location_2"]["type"])
-    #@location_3 = Location.new(name: params["location_3"]["name"], address: params["location_3"]["address"], city_id: @city.id, user_id: params[:id], guide_id: @guide.id, type: params["location_3"]["type"])
-
+    @location_3.save
     @params = params
     erb :"users/profile"
   end
+    
+    #if params[:figure].keys.include?("title_ids")
+      #params["figure"]["title_ids"].each do |item|
+        #@title = Title.find(item)
+        #@figure.titles << @title
+      #end
+    #end
   ######################################
   
-  post '/figures' do
-    @figure = Figure.create(name: params["figure"]["name"])
-    if params[:figure].keys.include?("title_ids")
-      params["figure"]["title_ids"].each do |item|
-        @title = Title.find(item)
-        @figure.titles << @title
-      end
-    end
-    if params["title"]["name"] != ""
-      @title = Title.create(name: params["title"]["name"])
-      @figure.titles << @title
-    end
-    #if !!params["landmark"]["name"].scan(/\w/)
+  #post '/figures' do
+    #@figure = Figure.create(name: params["figure"]["name"])
+    #if params[:figure].keys.include?("title_ids")
+      #params["figure"]["title_ids"].each do |item|
+        #@title = Title.find(item)
+        #@figure.titles << @title
+      #end
+    #end
+    #if params["title"]["name"] != ""
+      #@title = Title.create(name: params["title"]["name"])
+      #@figure.titles << @title
+    #end
+
     
-    if params["landmark"]["name"] != "" && params["landmark"]["name"] != nil
-      @landmark = Landmark.create(name: params["landmark"]["name"], year_completed: params["landmark"]["year_completed"], figure_id: @figure.id)
-      @landmark.save
-    end
-    if params["figure"]["landmark_ids"].length > 0
-      #binding.pry
-      params["figure"]["landmark_ids"].each do |id|
-        @landmark = Landmark.find(id)
-        #@landmark.update(figure_id: params[:id])
-        @landmark.update(figure_id: @figure.id)
-        @landmark.save
-      end
-    end
-    redirect to "/figures/#{@figure.id}"
-  end
+    #if params["landmark"]["name"] != "" && params["landmark"]["name"] != nil
+      #@landmark = Landmark.create(name: params["landmark"]["name"], year_completed: params["landmark"]["year_completed"], figure_id: @figure.id)
+      #@landmark.save
+    #end
+    #if params["figure"]["landmark_ids"].length > 0
+      
+      #params["figure"]["landmark_ids"].each do |id|
+        #@landmark = Landmark.find(id)
+        #@landmark.update(figure_id: @figure.id)
+        #@landmark.save
+      #end
+    #end
+    #redirect to "/figures/#{@figure.id}"
+  #end
   
   
   
