9ecd78743b431f455cd7e1302030b34753151fa4
Olivia Kesler <oakesler@protonmail.ch>
2020-01-19 06:43:03 +0000

    fixed back to profile route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
     erb :"users/edit"
   end
   
-  get '/users/:id/' do
+  get '/users/:id' do
     @user = User.find(params[:id])
     if !@user.name
       erb :"users/new"
