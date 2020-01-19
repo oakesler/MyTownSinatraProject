92108c937113366e4f08979b02f43a60bac9df52
Olivia Kesler <oakesler@protonmail.ch>
2020-01-19 07:15:42 +0000

    final fix for guide location relationship issue

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -3,6 +3,6 @@ class Guide < ActiveRecord::Base
   belongs_to :city
   has_many :locations
   ##############
-  has_many :guide_locations
-  has_many :locations, through: :guide_locations
+  #has_many :guide_locations
+  #has_many :locations, through: :guide_locations
 end
