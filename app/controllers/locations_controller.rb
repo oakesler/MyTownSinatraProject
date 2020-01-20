a1445b7a44263b27b550b6cf33f0ca2e626ef5af
Olivia Kesler <oakesler@protonmail.ch>
2020-01-20 23:09:54 +0000

    saving place for potentially functional location add method

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -29,10 +29,11 @@ class LocationsController < ApplicationController
       @name = Location.find(params["location_1"]["location_name_ids"][0]).name
     end
     if !!params["location_1"]["name"]
-      @name = Location.create(name: params["location_1"]["name"])
+      @name = "#{params["location_1"]["name"]}"
     end
 
     @location = Location.create(name: @name, address: params["location_1"]["address"], city_id: "#{@city.id}" , user_id: "#{@user.id}" , guide_id: "#{@guide.id}", description: params["location_1"]["description"], location_type_id: "#{@type.id}")
+    binding.pry
     erb :"guides/view"
   end
   
